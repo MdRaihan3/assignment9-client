fc1316e8c0c721cff234bb197d601f71c4ba40d4
MdRaihan3 <1mdr91012@gmail.com>
2024-04-17 09:37:16 +0600

    added animate

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,7 @@
 import { Helmet } from 'react-helmet-async';
-import './Contact.css'
+import 'animate.css'
+import './Contact.css';
+
 const Contact = () => {
     return (
         <div>
@@ -7,7 +9,7 @@ const Contact = () => {
                 <title>Contact</title>
             </Helmet>
             <div className=" contactt flex items-center justify-center my-7">
-                <p className=' text-6xl text-orange-500 font-bold'>Contact Us</p>
+                <p className=' text-6xl text-orange-500 font-bold animate__animated animate__backInDown'>Contact Us</p>
             </div>
             <div className=' grid grid-cols-2'>
                 <div className=' grid-cols-1 space-y-5 text-lg my-7'>
--- src/pages/Update/Update.jsx
+++ src/pages/Update/Update.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
 import { ToastContainer, toast } from 'react-toastify';
 import { Helmet } from 'react-helmet-async';
+import 'animate.css';
 
 const Update = () => {
     const {updateUserProfile} = useContext(AuthContext)
@@ -23,12 +24,12 @@ const Update = () => {
     return (
         <div>
              <Helmet>
-                <title>Update Profile</title>
+                <title >Update Profile</title>
             </Helmet>
         <div className="hero  bg-base-200 my-10">
             <div className="hero-content flex-col ">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Update Your Profile</h1>
+                    <h1 className="text-5xl font-bold  animate__animated animate__backInDown">Update Your Profile</h1>
                 </div>
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
 
